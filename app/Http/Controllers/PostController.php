193434284e03bc171927e076f4bd4cbe4310953a
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-11-07 13:24:08 +0900

    PostControllerにstoreメソッドを追加。(Eloquent ORM)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function create() {
         return view('post.create');
+    }
+
+
+    public function store(Request $request) {
+        $post = Post::create([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        // 処理後、元のページに戻る
+        return back();
     }
 }
